789f65151d00e3119b9357c5b229e2a6f5fe9aeb
Aniket24G <94295506+Aniket24G@users.noreply.github.com>
2023-12-30 19:55:45 +0530

    Final draft

--- Projects/Color-Changer/color.js
+++ Projects/Color-Changer/color.js
@@ -1,12 +1,33 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-buttons.forEach(function(button) {
-    console.log(button);
+buttons.forEach(function (button) {
+    // console.log(button);
+    button.addEventListener('click', function (e) {
+        // console.log(e);
+        // console.log(e.target);
+        let color = e.target.id;
+        switch (color) {
+            case 'pink':
+                body.style.backgroundColor = 'rgb(233, 120, 203)';
+                break;
+            case 'orange':
+                body.style.backgroundColor = 'rgb(227, 163, 96)';
+                break;
+            case 'blue':
+                body.style.backgroundColor = 'rgb(96, 228, 238)';
+                break;
+            case 'yellow':
+                body.style.backgroundColor = 'rgb(248, 248, 93)';
+                break;
+            default:
+                body.style.backgroundColor = 'white';
+                break;
+        }
 
-    button.addEventListener('click',function(e){
-        console.log(e);
-        console.log(e.target);
-        body.style.backgroundColor = e.target.id;
     });
 });
+
+function reset() {
+    body.style.backgroundColor = 'white';
+}
